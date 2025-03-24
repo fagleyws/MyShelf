4b9aa46c3932157f45ccf417105790d12701c7f0
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2025-03-24 16:01:58 -0500

    Registration

--- MyShelf_Web/Model/Registration.cs
+++ MyShelf_Web/Model/Registration.cs
@@ -5,16 +5,21 @@ namespace MyShelf_Web.Model
     public class Registration
     {
         [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
 
         [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
         [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm Password is required")]
         public string ConfirmPassword { get; set; }
     }
 }
--- MyShelf_Web/Pages/Account/Register.cshtml.cs
+++ MyShelf_Web/Pages/Account/Register.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using MyShelf_Web.Model;
 
 namespace MyShelf_Web.Pages.Account
@@ -13,9 +14,36 @@ namespace MyShelf_Web.Pages.Account
             //NewUser.FirstName = "John";
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
-            string firstName = NewUser.FirstName;
+            // Validate User Input
+            if (ModelState.IsValid)
+            {
+                // Save to Database
+                // 1. Create a connection to the database
+                string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=MyShelf;Trusted_Connection=True;";
+                SqlConnection conn = new SqlConnection(connectionString);
+                // 2. Create a command to insert the data
+                string cmdText = "INSERT INTO [User] (UserFirstName, UserLastName, UserProfileImage, UserEmail, UserPassword, AccountTypeID) VALUES (@FirstName, @LastName, @ProfileImage, @Email, @Password, 3)";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@FirstName", NewUser.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", NewUser.LastName);
+                cmd.Parameters.AddWithValue("@ProfileImage", "default.jpg");
+                cmd.Parameters.AddWithValue("@Email", NewUser.Email);
+                cmd.Parameters.AddWithValue("@Password", NewUser.Password);
+                // 3. Execute the command
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                // 4. Close the connection
+                conn.Close();
+
+                // Redirect to Login Page
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                return Page();
+            }
         }
-    }
+    }   
 }
