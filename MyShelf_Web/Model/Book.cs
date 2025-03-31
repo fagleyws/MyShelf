bada13d141d22d5ad088a7a896a464ac2239bf61
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2025-03-31 12:01:30 -0500

    Started AddBook page

---
+++ MyShelf_Web/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MyShelf_Web.Model
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string BookTitle { get; set; }
+        public string BookSummary { get; set; }
+        public string BookCoverImage { get; set; }
+        public decimal Price { get; set; }
+        public int PageCount { get; set; }
+        public string ISBN13 { get; set; }
+        public int AuthorID { get; set; }
+        public int PublisherID { get; set; }
+        public int FormatID { get; set; }
+        public int LanguageID { get; set; }
+        public int AddedBy { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
