90a763312c8694fcc1ebab0e792fa67ff9a30c31
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2025-04-09 14:37:59 -0500

    Created blank pages: BrowseBooks and EditBook

---
+++ MyShelf_Web/Pages/Books/EditBook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyShelf_Web.Pages.Books
+{
+    public class EditBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
